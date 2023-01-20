9672a17571c9be245db1a19c3917221f0610eb04
dane-art <danielfrsf@gmail.com>
2023-01-20 00:24:25 +0000

    Completo Jogo

--- Assets/Jogador.cs
+++ Assets/Jogador.cs
@@ -1,21 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class Jogador : MonoBehaviour
 {
 
     public float velocidade;
     private int contador = 0;
+    public  UnityEngine.UI.Text contar;
+    public  UnityEngine.UI.Text Vitoria;
 
      private Rigidbody rb;
     // Start is called before the first frame update
 void Start()
 {
     contador = 0;
+    Vitoria.text = "";
+    Contador();
+    
     //Fisica é adicionada ao componente Rigidbody
     //Guardar a referencia do objeto Rigidbody (se este axistir)
     rb = GetComponent<Rigidbody> ();
+
 }
     //O método FixedUpdate é chamado antes de ser efetuado qualquer cálculo de física.
     //Física deve ser adicionada neste método
@@ -37,10 +45,19 @@ void FixedUpdate ()
 private void OnTriggerEnter (Collider other)
 {
     contador = contador + 1;
+    Contador();
     //se o GameObject for um Coletável if (other.gameObject. CompareTag("Coletavel"))
     {
     //então desativa
     other.gameObject.SetActive(false);
+    }
+}
+
+void Contador()
+{
+    contar.text = "Contador: " + contador.ToString();
+    if(contador >= 7){
+    Vitoria.text = "Ganhou! Parabéns";
     }
 }
 
